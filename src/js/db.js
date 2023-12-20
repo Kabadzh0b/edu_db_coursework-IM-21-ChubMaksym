6fcc43d9cacc31daa788957c98c1d3533ae2218c
Kabadzh0b <chubbudda@gmail.com>
2023-12-20 21:00:42 +0200

    permission_has_role get and delete

--- src/js/db.js
+++ src/js/db.js
@@ -1,8 +1,9 @@
 import mysql from 'mysql2/promise';
 
 let connection;
-
+// Ініціалізація бази даних
 async function initializeDatabase() {
+    // Створення з'єднання з базою даних
   connection = await mysql.createConnection({
     host: 'localhost',
     user: 'Kabadzh0b',
@@ -14,26 +15,39 @@ async function initializeDatabase() {
 initializeDatabase();
 
 export default {
+    permission_has_role: {
+        read: async (roleID) => {
+            const [rows] = await connection.execute('SELECT * FROM permission_has_role WHERE Permission_id = ?', [roleID]);
+            return rows;
+        },
+        delete: async (permissionID) => {
+            await connection.execute('DELETE FROM permission_has_role WHERE Permission_id = ?', [permissionID]);
+        }
+    },
     permission: {
+        // Створення нового дозволу
         create: async (name) => {
             const [result] = await connection.execute('INSERT INTO permission (name) VALUES (?)', [name]);
             return result.insertId;
         },
 
+        // Читання дозволу за id
         read: async (id) => {
             const [rows] = await connection.execute('SELECT * FROM permission WHERE id = ?', [id]);
             return rows[0];
         },
-        
+        // Читання всіх дозволів
         readAll: async () => {
             const [rows] = await connection.execute('SELECT * FROM permission');
             return rows;
         },
 
+        // Оновлення дозволу за id
         update: async (id, name) => {
             await connection.execute('UPDATE permission SET name = ? WHERE id = ?', [name, id]);
         },
 
+        // Видалення дозволу за id
         delete: async (id) => {
             await connection.execute('DELETE FROM permission WHERE id = ?', [id]);
         }
--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,40 @@ import db from "./db.js";
 
 const server = fastify();
 
+// Отримання всіх дозволів
 server.get('/permission', async (request, reply) => {
     const permissions = await db.permission.readAll();
     reply.send(permissions);
 });
 
+// Отримання дозволу за id
 server.get('/permission/:id', async (request, reply) => {
     const { id } = request.params;
     const permission = await db.permission.read(id);
     reply.send(permission);
 });
 
+// Створення нового дозволу
 server.post('/permission', async (request, reply) => {
     const { name } = request.body;
     const id = await db.permission.create(name);
     reply.send({ id });
 });
-  
+
+// Отримання ролей, що мають цей дозвіл
+server.get('/permission_has_role/:roleID', async (request, reply) => {
+    const { roleID } = request.params;
+    const roles = await db.permission_has_role.read(roleID);
+    reply.send(roles);
+});
+
+// Видалення ролей, що мають цей дозвіл
+server.delete('/permission_has_role/:permissionID', async (request, reply) => {
+    const { permissionID } = request.params;
+    await db.permission_has_role.delete(permissionID);
+    reply.send({ success: true });
+});
+// Оновлення дозволу за id
 server.put('/permission/:id', async (request, reply) => {
     const { id } = request.params;
     const { name } = request.body;
@@ -27,12 +44,14 @@ server.put('/permission/:id', async (request, reply) => {
     reply.send({ success: true });
 });
   
+// Видалення дозволу за id
 server.delete('/permission/:id', async (request, reply) => {
     const { id } = request.params;
     await db.permission.delete(id);
     reply.send({ success: true });
 });
   
+// Запуск сервера
 server.listen(3000, (err, address) => {
     if (err) throw err;
     console.log(`Server listening on ${address}`);
